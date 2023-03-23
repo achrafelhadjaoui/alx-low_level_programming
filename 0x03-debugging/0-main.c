e58035ea34d4034c77a9bb9568ce6ffef345e9f5
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2023-03-23 08:06:06 -0700

    if the function gives the correct output when given a case of 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * test_func - a function that test a function
+ * @k: the only parameter in form of int
+ *
+ * Return: 1 if the function correct
+ */
+int test_func(int k)
+{
+	k = positive_or_negative(i);
+	if (i == 0 && k == 0)
+		return (1);
+}
