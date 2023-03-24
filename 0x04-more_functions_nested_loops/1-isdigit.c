0604c3151546d3afd3c39262d79d6fb174f5044a
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2023-03-23 19:02:07 -0700

    is a digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,9 +7,9 @@
  * Return: 1 if c is a digit, and 0 otherwise
  */
 
-int _isupper(int c)
+int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
