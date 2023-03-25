d564621d176728ea7a521675ecc7b40278e1c367
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2023-03-24 18:34:10 -0700

    functions that draws

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - function that draw
+ * @n: the parameter in form of int
+ */
+
+void print_line(int n)
+{
+	int num;
+
+	num = 0;
+	while (num <= n)
+	{
+		_putchar('_');
+		num++;
+	}
+	_putchar('\n');
+}
