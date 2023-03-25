7b2158f5f9bcc82def50443f6ee625e4e3a235c1
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2023-03-24 18:44:20 -0700

    a function that draws

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function that draws
+ * @n: the parameter in form of int
+ */
+
+void print_diagonal(int n)
+{
+	int num;
+
+	num = 0;
+	while (num < n)
+	{
+		_putchar('\\');
+		num++;
+	}
+	_putchar('\n');
+}
