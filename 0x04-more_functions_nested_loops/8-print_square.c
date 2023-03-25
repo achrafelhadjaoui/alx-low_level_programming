b1d4662dcffa5430cfa60b24e9d07e4c6abfb3b5
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2023-03-24 19:52:49 -0700

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - function that draws
+ * @size: the parameter in form of int
+ */
+
+void print_square(int size)
+{
+	int num;
+	int dist;
+
+	num = 0;
+	while (num < size)
+	{
+		dist = 0;
+		while (dist <= size)
+		{
+			_putchar('\\');
+			dist++;
+		}
+		_putchar('\n');
+		num++;
+	}
+}
