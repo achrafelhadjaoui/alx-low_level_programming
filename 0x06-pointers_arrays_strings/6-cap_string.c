1ee1405bfca83097384901ddda0ba04bd17e80e9
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2023-03-30 16:31:23 -0700

    update

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,41 +1,12 @@
 #include "main.h"
 
 /**
- * cap_string - a function that capitalizes
- * all words of a string
- * @str: the parameter
+ * _to_lower - a healper function that convert every
+ * to lowercase
+ * @s: the parameter
  *
- * Return: str.
+ * Return: s
  */
-
-char *_to_lower(char *s);
-
-char *cap_string(char *str)
-{
-	int i;
-	char *p;
-
-	p = _to_lower(str);
-	i = 0;
-	if (p[i] >= 97 && p[i] <= 122)
-		p[i] = p[i] - 32;
-	while (p[i] != '\0')
-	{
-		if (p[i] == 9)
-			p[i] = p[i] + 23;
-		if ((p[i] >= 9 && p[i] <= 10) || (p[i] >= 32 && p[i] <= 34) 
-			|| (p[i] >= 40 && p[i] <= 41) || (p[i] == 44) || (p[i] == 46) 
-			|| (p[i] == 59) || (p[i] == 63) || (p[i] == 123) || (p[i] == 125))
-		{
-			if (p[i + 1] >= 97 && p[i + 1] <= 122)
-				p[i + 1] = p[i + 1] - 32;
-			i++;
-		}
-		else
-			i++;
-	}
-	return (p);
-}
 
 char *_to_lower(char *s)
 {
@@ -53,4 +24,39 @@ char *_to_lower(char *s)
 			j++;
 	}
 	return (s);
+}
+
+/**
+ * cap_string - a function that capitalizes
+ * all words of a string
+ * @str: the parameter
+ *
+ * Return: str.
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+	char *p;
+
+	p = _to_lower(str);
+	i = 0;
+	if (p[i] >= 97 && p[i] <= 122)
+		p[i] = p[i] - 32;
+	while (p[i] != '\0')
+	{
+		if (p[i] == 9)
+			p[i] = p[i] + 23;
+		if ((p[i] >= 9 && p[i] <= 10) || (p[i] >= 32 && p[i] <= 34)
+			|| (p[i] >= 40 && p[i] <= 41) || (p[i] == 44) || (p[i] == 46)
+			|| (p[i] == 59) || (p[i] == 63) || (p[i] == 123) || (p[i] == 125))
+		{
+			if (p[i + 1] >= 97 && p[i + 1] <= 122)
+				p[i + 1] = p[i + 1] - 32;
+			i++;
+		}
+		else
+			i++;
+	}
+	return (p);
 }
