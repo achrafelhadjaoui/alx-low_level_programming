bdce6c32512d179e4bcd3da49ce66e08d8329c58
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2023-04-01 20:14:29 -0700

    print exactly a string

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - is the function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora korapar, 2015-10-19");
+	return (0);
+}
