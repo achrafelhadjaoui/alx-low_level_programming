c3c6f115c6d986d10f68058c477e54379bb8fab0
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2023-04-03 17:55:52 -0700

    fill the memory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _memset - a function that fills a memory with a constant byte
+ * @s: this is a pointer of the memory location
+ * @b: this is the parameter that hold the the value that is
+ * to be copied
+ * @n: this is size which will fill
+ *
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (i < n)
+		{
+			s[i] = b;
+		}
+		i++;
+	}
+	return (s);
+}
