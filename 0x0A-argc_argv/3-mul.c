aaf7cf76cf75be6f0f0b8e4ef3d2c0d1f049cae7
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2023-04-21 16:44:58 -0700

    multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdio.h>
+
+int _toint(char *n);
+
+/**
+ * main - a program that print its name.
+ * @argc: a variable refers to the number of arguments passed
+ * @argv: is a pointer array which points to each argument passed to the
+ * program.
+ *
+ * Return: 0.
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", _toint(argv[1]) * _toint(argv[2]));
+	}
+	return (0);
+}
+
+/**
+ * _toint - a function that converts charcter to int.
+ * @n: the address that holds the given characters.
+ *
+ * Return: the cnverted int.
+ */
+
+int _toint(char *n)
+{
+	int i;
+	int res;
+
+	i = 0;
+	res = 0;
+	while (n[i] != '\0')
+	{
+		res = (res * 10) + (n[i] - 48);
+		i++;
+	}
+	return (res);
+}
