b8621389ae506f4825aeb4cbd0edb6a4ec43b675
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2023-04-21 16:49:46 -0700

    multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,11 +14,13 @@ int _toint(char *n);
 
 int main(int argc, char **argv)
 {
-	if (argc < 3)
+	if (argc < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	else if (argc < 3)
+		printf("%s\n", argv[1]);
 	else
 	{
 		printf("%d\n", _toint(argv[1]) * _toint(argv[2]));
