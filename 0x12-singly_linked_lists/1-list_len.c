b0f991469a9febad6463081eeb68db69f758ae13
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2023-04-28 16:13:47 -0700

    number of elements

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - a function that returns the number of elements
+ * @h: a pointer
+ *
+ * Return: the number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t i;
+
+	i = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
