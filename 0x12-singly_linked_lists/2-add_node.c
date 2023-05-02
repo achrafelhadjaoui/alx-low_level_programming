f6630c3a84e47831715dd4e1d0b9fa89c01fb99b
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2023-05-01 17:34:05 -0700

    add

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -19,10 +19,6 @@ list_t *add_node(list_t **head, const char *str)
 	if (ptr == NULL)
 		return (NULL);
 	ptr->str = strdup(str);
-	if (ptr->str == NULL)
-	{
-		return (NULL);
-	}
 	while (str[i] != '\0')
 		i++;
 	ptr->len = i;
