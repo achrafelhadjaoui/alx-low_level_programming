b7de37eccebc4b035e0b9ca8b56ac7bb6a2327d8
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2023-05-03 15:51:34 -0700

    free list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - a function yhat frees a list
+ * @head: points to node.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+	listint_t *i;
+
+	ptr = *head;
+	while (ptr != NULL)
+	{
+		i = ptr->next;
+		free(ptr);
+		ptr = i;
+	}
+	*head = NULL;
+}
