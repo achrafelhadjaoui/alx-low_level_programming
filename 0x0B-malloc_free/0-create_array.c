9a69052e966e860f4a43162eaacbb40678bc685d
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2023-06-14 11:45:05 -0700

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,16 +12,18 @@
 char *create_array(unsigned int size, char c)
 {
 	char *ptr;
+	int i;
 
+	i = 0;
 	if (size == 0)
 		return (NULL);
 	ptr = malloc(sizeof(char) * size);
 	if (ptr == NULL)
 		return (NULL);
-	while (*ptr != '\0')
+	while (i < size)
 	{
-		*ptr = c;
-		ptr++;
+		ptr[i] = c;
+		i++;
 	}
 	return (ptr);
 }
