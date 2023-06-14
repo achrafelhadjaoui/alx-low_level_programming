7eb81afa2b5543064caf5d4040e2a92a4e18af6e
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2023-06-14 11:57:49 -0700

    updare

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,6 +15,8 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	i = 0;
+	if (size == 0)
+		return (NULL);
 	ptr = malloc(sizeof(char) * size);
 	if (ptr == NULL)
 		return (NULL);
