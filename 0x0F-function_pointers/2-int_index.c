0e7441c8216356ef845987387ba70f1050cb25f9
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2023-06-23 18:45:42 -0700

    update

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,7 +16,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	i = 0;
-	if (array == NULL && cmp == NULL)
+	if (array == NULL || cmp == NULL)
 		return (-1);
 	while (i < size)
 	{
