cedd5bbc6859a01d635d724f45510372ecd73e8d
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2023-08-04 14:39:58 -0700

    arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - the function
+ * @argv: first variable
+ * @argc: second variable
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+int change(char *c);
+
+int mul(int ope1, int ope2);
+
+int cheek(char *number);
+
+int change_to_int(char *nm);
+
+#endif
