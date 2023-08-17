ef310723ec914e9ec86dea689a580b4e22242884
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2023-08-17 12:34:08 -0700

    update

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 		printf("(nil)\n");
 	else
 		printf("%s\n", d->name);
-	if (d->age == < 0)
+	if (d->age < 0)
 		printf("(nil)\n");
 	else
 		printf("%f\n", d->age);
