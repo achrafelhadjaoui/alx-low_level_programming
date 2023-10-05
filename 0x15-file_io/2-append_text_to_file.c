c17eacbf4eb8a30f257df868548f1fb1a05b9686
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2023-10-05 12:22:06 -0700

    update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,7 +22,10 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	else if (text_content == NULL)
+	{
+		close(fd);
 		return (1);
+	}
 
 	else if (text_content != NULL)
 		dp = dprintf(fd, "%s", text_content);
