b6e6e134d136028947b429bd9fa22f7fd8318d55
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2023-10-06 15:14:04 -0700

    Update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,27 @@
 #include "main.h"
 
 /**
+ * check_error - a function that looks for errors and print them into stderr
+ * @file_from: the first parameter
+ * @file_to: the second parameter
+ * @av: the third parameter
+ */
+
+void check_error(int file_from, int file_to, char *av)
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av);
+		exit(99);
+	}
+}
+
+/**
  * main - the main function
  * @ac: the argument count
  * @av: the argument vector
@@ -21,36 +42,16 @@ int main(int ac, char *av[])
 	}
 
 	file_from = open(av[1], O_RDONLY);
-
-	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
+	check_error(file_from, 0, av[1]);
 
 	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-
-	}
+	check_error(0, file_to, av[2]);
 
 	while ((check_read = read(file_from, buff, 1024)) > 0)
 	{
 		check_write = write(file_to, buff, check_read);
-		if (check_write == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-			exit(99);
-		}
 	}
-
-	if (check_read == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
+	check_error(check_read, 0, av[1]);
 
 	if (close(file_from) == -1)
 	{
