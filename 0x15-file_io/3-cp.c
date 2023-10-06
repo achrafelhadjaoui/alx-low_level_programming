51bec852fc856f898de3605ad49d5d0031141322
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2023-10-06 10:22:52 -0700

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,21 +21,35 @@ int main(int ac, char *av[])
 	}
 
 	file_from = open(av[1], O_RDONLY);
-	check_read = read(file_from, buff, 1024);
 
-	if (file_from == -1 || check_read == -1)
+	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 
-	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 00664);
-	check_write = write(file_to, buff, check_read);
-
-	if (file_to == -1 || check_write == -1)
+	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
+
+	}
+
+	while ((check_read = read(file_from, buff, 1024)) > 0)
+	{
+		check_write = write(file_to, buff, check_read);
+		if (check_write == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	}
+
+	if (check_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
 	}
 
 	if (close(file_from) == -1)
